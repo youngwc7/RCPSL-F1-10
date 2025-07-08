9c0659ac20884759a3b836ce9cea2bb5b0e3ba16
formulazot <youngwc7@uci.edu>
2025-07-08 15:42:54 -0700

    Wall-PID

---
+++ src/move.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64
+import math
+
+class CmdVelToVesc:
+    def __init__(self):
+        # Parameters
+        self.center_angle = 0.5
+        self.steering_angle = self.center_angle
+
+        # Servo Specs
+        self.max_angle = 30.0
+        self.max_angle_rad = math.radians(self.max_angle)
+
+        rospy.init_node('cmdvel_to_vesc')
+
+        self.motor_pub = rospy.Publisher(
+            '/commands/motor/duty_cycle', Float64, queue_size=1)
+        self.servo_pub = rospy.Publisher(
+            '/commands/servo/position', Float64, queue_size=1)
+
+        rospy.Subscriber('/cmd_vel', Twist, self.cmd_vel_callback)
+
+    def clamp(self, val, min_val, max_val):
+        return max(min(val, max_val), min_val)
+
+    def cmd_vel_callback(self, msg):
+        duty = self.clamp(msg.linear.x * 0.2, -0.2, 0.2)
+        
+        # Incremental steering control
+        #step = 0.05
+        #if msg.angular.z > 0.1:
+        #    self.steering_angle += step
+        #elif msg.angular.z < -0.1:
+        #    self.steering_angle -= step
+        #else:
+        #    pass  # optionally recenter
+        
+        #if abs(msg.angular.z - 99.0) < 1e-2:
+        #    self.steering_angle = 0.5
+
+        self.steering_angle = msg.angular.z
+        # servo motor seems to be mounted opposite. 
+        # 0.5 ~ 1 -> 0 ~ 0.5
+        # 0 ~ 0.5 -> 0.5 ~ 1
+        # self.steering_angle = (self.steering_angle - 0.5) * -1.0 + 0.5
+        self.steering_angle = self.clamp(self.steering_angle, 0.0, 1.0)
+
+        rospy.loginfo("Duty=%.2f, Steering=%.2f", duty, self.steering_angle)
+
+        self.motor_pub.publish(Float64(data=duty))
+        self.servo_pub.publish(Float64(data=self.steering_angle))
+
+    def spin(self):
+        rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        node = CmdVelToVesc()
+        node.spin()
+    except KeyboardInterrupt:
+        pass
---
+++ src/wall_follow.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import rospy
+import math
+import numpy as np
+from sensor_msgs.msg import LaserScan
+from ackermann_msgs.msg import AckermannDriveStamped
+from geometry_msgs.msg import Twist
+
+DEG_TO_RAD = math.pi / 180.0
+MIN_ANGLE = 50
+MAX_ANGLE = 90
+PERPENDICULAR = 90 * DEG_TO_RAD
+
+class WallFollowNode:
+    def __init__(self):
+        rospy.init_node('wall_follow_node', anonymous=True)
+
+        # Wall-following parameters
+        self.L = 1.0  # lookahead distance
+        
+        # min distance from wall desired (narrowest part of track). 
+        if not rospy.has_param("~distance_setpoint"):
+            rospy.logwarn("Distance setpoint not given. Using default (1.0)")
+            self.distance_setpoint = 1.0
+        else:
+            self.distance_setpoint = rospy.get_param("~distance_setpoint")
+
+        self.out_of_range = self.distance_setpoint * 5
+
+        # Angles for the two lidar beams (RIGHT WALL)
+        self.a_angle = -1 * MIN_ANGLE * DEG_TO_RAD
+        self.b_angle = -1 * MAX_ANGLE * DEG_TO_RAD
+        self.theta = 40 * DEG_TO_RAD
+
+        # PID coefficients
+        if not rospy.has_param("~kp"):
+            rospy.logwarn("PIDs in launch file incomplete. Using defaults.")
+
+        self.kp = rospy.get_param("~kp", 100.0)
+        self.ki = rospy.get_param("~ki", 1.0)
+        self.kd = rospy.get_param("~kd", 5.0)
+
+        # PID state
+        self.prev_error = 0.0
+        self.integral_error = 0.0
+        self.prev_time = None
+
+        # Velocity limits
+        self.straight_vel = 0.6
+        self.low_turn_vel = 0.40
+        self.med_turn_vel = 0.30
+        self.high_turn_vel = 0.25
+
+        self.drive_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+        self.scan_sub = rospy.Subscriber("/scan", LaserScan, self.scan_callback)
+
+    def get_range(self, scan_msg, angle):
+        """
+        Return the range measurement at a given angle.
+        """
+        # clip angle to sensor bounds
+        if angle < scan_msg.angle_min:
+            angle = scan_msg.angle_min
+        elif angle > scan_msg.angle_max:
+            angle = scan_msg.angle_max
+
+        idx = int((angle - scan_msg.angle_min) / scan_msg.angle_increment)
+        idx = max(0, min(idx, len(scan_msg.ranges)-1))
+
+        distance = scan_msg.ranges[idx]
+        if math.isinf(distance) or math.isnan(distance):
+            return scan_msg.range_max
+        else:
+            return distance
+    
+    # get aggregate error (of one side of the scans, either left or right)
+    def get_agg_error(self, scan_msg, min_angle, max_angle):
+        angle = min_angle
+        D_t = 0.0
+        D_t_plus_lookahead = 0.0
+
+        num = abs(int((max_angle - min_angle) / scan_msg.angle_increment))
+        ortho_scan = self.get_range(scan_msg, PERPENDICULAR)
+        
+        while angle <= max_angle:
+            tmp = self.get_range(scan_msg, angle)
+            if tmp > self.out_of_range:
+                angle += scan_msg.angle_increment
+                num -= 1
+                continue
+            
+            alpha = math.atan2(tmp * math.cos(PERPENDICULAR - abs(angle)), tmp * math.sin(PERPENDICULAR - abs(angle)))
+            
+            tmp_d_t = tmp * math.cos(alpha)            
+            tmp_d_t_plus_lookahead = tmp_d_t + self.L * math.sin(alpha)
+          
+            D_t += tmp_d_t
+            D_t_plus_lookahead += tmp_d_t_plus_lookahead
+            
+            angle += scan_msg.angle_increment
+        
+        avg_D_t = D_t / num
+        avg_D_t_plus_lookahead = D_t_plus_lookahead / num
+        error = self.distance_setpoint - avg_D_t_plus_lookahead
+        
+        return error, avg_D_t, avg_D_t_plus_lookahead, num
+
+    	    	
+    
+    def get_error(self, scan_msg):
+        a = self.get_range(scan_msg, self.a_angle)
+        b = self.get_range(scan_msg, self.b_angle)
+
+        alpha = math.atan2(a * math.cos(self.theta) - b,
+                           a * math.sin(self.theta))
+
+        D_t = b * math.cos(alpha)
+        D_t_plus_1 = D_t + self.L * math.sin(alpha)
+
+        error = self.distance_setpoint - D_t_plus_1
+        rospy.loginfo("pid err: %f", error)
+        return error
+
+    def pid_control(self, error, current_time):
+        if self.prev_time is None:
+            delta_t = 0.0
+        else:
+            delta_t = current_time - self.prev_time
+
+        derivative_error = (error - self.prev_error) / delta_t if delta_t > 0 else 0.0
+        self.integral_error += error * delta_t
+
+        angle = (self.kp * error +
+                 self.ki * self.integral_error +
+                 self.kd * derivative_error)
+
+        rospy.loginfo("pid angle: %f", angle)
+        # limit angle to ±30 deg
+        angle = max(min(angle, 30), -30)
+        self.prev_error = error
+        self.prev_time = current_time
+        
+        angle = (angle * -1 + 30.0) / 60.0
+        
+        return angle
+
+    def velocity_limiter(self, angle):
+        abs_angle = abs(angle)
+        
+        if 0 < abs_angle < 5.0:
+            return self.straight_vel
+        elif 5.0 <= abs_angle < 13.0:
+            return self.low_turn_vel
+        elif 13.0  <= abs_angle < 20.0:
+            return self.med_turn_vel
+        else:    
+            return self.high_turn_vel
+
+    def scan_callback(self, scan_msg):
+        # error = self.get_error(scan_msg)
+        left_error, _ , _, _ = self.get_agg_error(scan_msg, MIN_ANGLE * DEG_TO_RAD, MAX_ANGLE * DEG_TO_RAD)
+        # right side's angles become inverted 
+        right_error, _, _, _ = self.get_agg_error(scan_msg, -1 * MAX_ANGLE * DEG_TO_RAD, -1 * MIN_ANGLE * DEG_TO_RAD)
+        rospy.loginfo("LEFT_ERR: % .3f   RIGHT_ERR: % .3f", left_error, right_error)
+
+        # SUS.
+        ## error = left_error - right_error
+        error = right_error - left_error
+        rospy.loginfo("COMBINED ERR: % .3f", error)
+        
+        current_time = scan_msg.header.stamp.to_sec()
+
+        angle = self.pid_control(error, current_time)
+        velocity = self.velocity_limiter(angle)
+
+        drive_msg = Twist()
+        drive_msg.angular.z = angle
+        drive_msg.linear.x = velocity
+
+        rospy.loginfo_throttle(1.0, f"Velocity: {velocity:.2f} m/s, Steering Angle: {math.degrees(angle):.2f} deg")
+
+        self.drive_pub.publish(drive_msg)
+
+
+if __name__ == "__main__":
+    try:
+        node = WallFollowNode()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
+
